8e656d30c68cec25758a61e4d8781a60ebd40e8e
jonas.gonzalez@iit.it <jonas.gonzalez@iit.it>
2018-03-30 14:56:23 +0200

    add git ignore to project

--- src/ObjectDetectionThread.cpp
+++ src/ObjectDetectionThread.cpp
@@ -191,7 +191,7 @@ void ObjectDetectionThread::drawDetectedBoxes(IplImage *t_imageToDraw) {
         originBox = cvPoint(it.second.coordinate[0], it.second.coordinate[1]);
         endBox = cvPoint(it.second.coordinate[2], it.second.coordinate[3]);
         const Color objectColor = getObjectColor(it.second.className);
-        cvRectangle(t_imageToDraw, originBox, endBox, cvScalar(objectColor.red, objectColor.green, objectColor.blue), 3);
+        cvRectangle(t_imageToDraw, originBox, endBox, cvScalar(objectColor.red, objectColor.green, objectColor.blue), 2);
 
         displayText = cvPoint(it.second.coordinate[0], it.second.coordinate[1] - 6);
         cv::putText(cv::cvarrToMat(t_imageToDraw), it.first, displayText, CV_FONT_HERSHEY_TRIPLEX, 0.5, cvScalar(objectColor.red, objectColor.green, objectColor.blue), 1);
